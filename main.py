41c6403f633cc023217a519cfeb67ae2008b1f4f
jisuAn <ajs7270@snu.ac.kr>
2023-05-25 17:03:12 +0900

    Add test code

---
+++ main.py
@@ -0,0 +1,54 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from langchain import OpenAI, ConversationChain
+
+from datasets import Dataset
+from core import PoT
+
+# Load SVAMP dataset
+svamp = Dataset(Path("data/SVAMP.json"))
+
+# Load LlamaCpp
+llm = OpenAI(temperature=0.7)
+
+@dataclass
+class Result:
+    passage: str
+    question: str
+    answer: float
+    openai: str
+
+def dataclass_to_dict(obj):
+    if isinstance(obj, list):
+        return [dataclass_to_dict(value) for value in obj]
+    elif isinstance(obj, dict):
+        return {key: dataclass_to_dict(value) for key, value in obj.items()}
+    elif isinstance(obj, Result):
+        return {"passage": obj.passage, "question": obj.question, "answer": obj.answer, "openai": obj.openai}
+    else:
+        return obj
+
+PoT_correct = 0
+outputs = []
+for i, problem in enumerate(svamp):
+    pot_output = PoT(llm=llm, problem=problem)
+
+    if pot_output == problem.answer:
+        PoT_correct += 1
+
+    outputs.append(pot_output)
+
+    print(f"current corrects PoT: {PoT_correct}")
+    outputs.append(Result(passage=problem.passage, question=problem.question, answer=problem.answer, openai=pot_output))
+    break
+
+# Convert dataclass objects to dictionaries
+outputs = dataclass_to_dict(outputs)
+
+# Save result json
+with open(Path(f"result.json"), 'w') as f:
+    json.dump({
+        "PoT correct": PoT_correct,
+        "Results": outputs,
+    }, f, indent=4)
